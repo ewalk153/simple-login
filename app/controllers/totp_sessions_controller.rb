d67b502f65910b9fd5f176919f08bffcd7efc5fc
Eric Walker <eric.walker@shopify.com>
2022-01-11 21:05:35 -0500

    Redirect away from totp session when fully auth'd

--- app/controllers/totp_sessions_controller.rb
+++ app/controllers/totp_sessions_controller.rb
@@ -1,5 +1,6 @@
 class TotpSessionsController < ApplicationController
   def new
+    redirect_to root_url if session[:totp_confirmed]
     @totp = ROTP::TOTP.new(totp_secret, issuer: User::TOTP_ISSUER)
   end
 
